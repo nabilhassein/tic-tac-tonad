0fe6ee122cabfaafc5eeb159a42eddd3ac932bfe
katherineye <kye@princeton.edu>
2013-07-29 18:03:54 -0400

    error checking and handling works

--- ttt.hs
+++ ttt.hs
@@ -1,9 +1,6 @@
 import Data.List
 import qualified Data.Map as Map
-
-type Board = Map.Map Int Char
-type Side = Char
-type Position = Int
+import Data.Maybe
 
 {-
 config = [['0', '1', '2'], 
@@ -15,8 +12,9 @@ board = [['*', '*', '*'],
 		 ['*', '*', '*']]
 -}
 
-toBoard :: [Char] -> Map.Map Int Char
-toBoard = Map.fromList . zip [0..8]
+type Board = Map.Map Int Char
+type Side = Char
+type Position = Int
 
 config :: Board
 config = toBoard ['0'..'8']
@@ -24,8 +22,12 @@ config = toBoard ['0'..'8']
 initialBoard :: Board
 initialBoard = toBoard $ replicate 9 '*'
 
+toBoard :: [Char] -> Map.Map Int Char
+toBoard = Map.fromList . zip [0..8]
+
 -- refuse to use fold. make this shorter?
--- maybe write a function to convert to [[Char]], then prettyPrint as before
+-- maybe write a function to convert to [Char] or [[Char]], 
+-- then prettyPrint as before
 prettyPrint :: Board -> String
 prettyPrint board = 
 	map snd $
@@ -33,7 +35,7 @@ prettyPrint board =
 	++ ((-1, '\n') : (filter (inRange 3 5) list))
 	++ ((-1, '\n') : (filter (inRange 6 8) list))
 		where list = Map.toList board;
-			inRange lower upper (k, v) = lower <= k && k <= upper
+			  inRange lower upper (k, v) = lower <= k && k <= upper
 
 -- maps have strange update / alter functions
 addMove :: Side -> Position -> Board -> Board
@@ -48,7 +50,7 @@ main = do
 	putStrLn $ prettyPrint config ++ "\n"
 	putStrLn $ "here's the board" ++ "\n"
 	putStrLn $ prettyPrint initialBoard ++ "\n"
-	xmove (return initialBoard)
+	xmove (return config)
 
 xmove :: IO Board -> IO ()
 xmove board = do
@@ -71,19 +73,30 @@ getMove side board = do
 	putStrLn $ (side : "'s move. enter number of position")
 	(pos :: Int) <- fmap read getLine
 	boardResult <- board 
-	-- can i fix this? lots of wrapping, unwrapping boards
-	let newBoard = addMove side pos boardResult
-	return newBoard
+
+	let value = Map.lookup pos boardResult;
+		checkMove = fromMaybe '-' value
+
+	if checkMove == '-'
+		then do
+			putStrLn $ "\n" ++ "error: out of bounds" ++ "\n"
+			getMove side board
+		else if checkMove == 'X' || checkMove == 'O'
+			then do
+				putStrLn $ "\n" ++ "error: moved in an occupied space" ++ "\n"
+				getMove side board
+			else do -- valid move
+				let newBoard = addMove side pos boardResult;
+				return newBoard
 
 	
 
 
 
 -- todo: 
--- check if move space taken or out of bounds, loop
 -- check for win
--- fix -XScopedTypeVariables
 -- write AI
--- board representation: ADT, [[Char]], Map Int Char?
+-- fix -XScopedTypeVariables
+-- learn about do notation (bind, why considered harmful)
 
 
