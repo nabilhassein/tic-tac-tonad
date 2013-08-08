32bf8c397abb51b10b5d6b58b2eac60751ad26cd
katherineye <kye@princeton.edu>
2013-08-08 14:52:03 -0400

    wrote aio2, but there are multiple problems - runhaskell -XScopedTypeVariables -XFlexibleInstances -XTypeSynonymInstances -XOverlappingInstances aio2 - plays always in lower right corner again - very slow for empty board, possibly not efficient

--- aio2.hs
+++ aio2.hs
@@ -8,40 +8,39 @@ import Debug.Trace
 
 import Utility
 
+-- would like to do: type SidedBoard = (Side, Board), type decideFunc = ? (possible?)
+-- anyway, -XFlexibleInstances
 instance Ord (Side, Board) where
-	compare (s1, b1) (s2, b2) = 
-		compare (utility s1 b1) (utility s2 b2) 
-
--- MINIMAX
-
--- how to write compose2 ::  (a -> b -> c) -> (c -> d) -> (a -> b -> d)
-minimaxDecision :: Side -> Board -> Board 
-minimaxDecision side board = 
-	maximum $ enumerate side board 
-
-
-maxVal :: 
-maxVal 
-
-
-minVal ::
-minVal 
-
+	compare sb1 sb2 = 
+		compare (utility sb1) (utility sb2) 
 
 --- MISC FUNCTIONS
 
-utility :: Side -> Board -> Int 
-utility side board 
+-- uh-oh. are sides/boards being played and enumerated and utility-asssessed correctly
+
+utility :: (Side, Board) -> Int 
+utility sidedBoard@(side, board)
 	| won side board = 1
 	| won (opposite side) board = -1
-	| otherwise = 0 
+	| full board = 0 
+	| otherwise = utility $ minimaxDecision (opposite side, board)
 
-enumerate :: Side -> Board -> [(Side, Board)]
-enumerate side board =
+decideFunc :: (Ord a) => Side -> ([a] -> a)
+decideFunc 'I' = maximum
+decideFunc 'O' = minimum
+
+enumerate :: (Side, Board) -> [(Side, Board)]
+enumerate (side, board) =
 	zip [side, side..] $ map (addMove side board) $ filter (posIsEmpty board) [0..8]
 
+-- MINIMAX
+
+minimaxDecision :: (Side, Board) -> (Side, Board)
+minimaxDecision sidedBoard@(side, board) = 
+	decideFunc side $ enumerate sidedBoard
+
 makeAImove :: Side -> Board -> Board 
-makeAImove = minimaxDecision
+makeAImove side board = snd $ minimaxDecision (side, board) 
 
 
 --- DEBUGGING CODE
@@ -50,10 +49,10 @@ makeAImove = minimaxDecision
 const_side :: Side 
 const_side = 'I'
 
-const_board0 :: board 
+const_board0 :: Board 
 const_board0 = 
 	Map.fromList 
-	[(0 :: Int, '*'), (1 :: Int, '*'), (2 :: Int, '*'), 
+	[(0 :: Int, 'I'), (1 :: Int, 'I'), (2 :: Int, '*'), 
 	 (3 :: Int, '*'), (4 :: Int, '*'), (5 :: Int, '*'), 
 	 (6 :: Int, '*'), (7 :: Int, '*'), (8 :: Int, '*')]
 
@@ -63,9 +62,10 @@ main :: IO ()
 main = do
 	putStrLn $ "side: " ++ [const_side] ++ "\n"
 	putStrLn $ "initial board: " ++ "\n"
-	putStrLn $ prettyPrint const_board1 ++ "\n\n" ++ "-----" ++ "\n"
+	putStrLn $ prettyPrint const_board0
+	 ++ "\n\n" ++ "-----" ++ "\n"
 	let bestBoard = makeAImove const_side const_board0
-	prettyPrint bestBoard
+	putStrLn $ prettyPrint bestBoard
 
 
 
